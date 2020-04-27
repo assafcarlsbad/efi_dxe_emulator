459652a80757a558024ca9cf5e67e6d3af51152b
Assaf Carlsbad <carlsbad@sentinelone.com>
2020-04-28 02:15:52 +0300

    Add some more definitions to types.h

--- efi_dxe_emulator/types.h
+++ efi_dxe_emulator/types.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #ifdef _WIN32
-
+#ifndef GUID_DEFINED
+#include <guiddef.h>
+#endif // !GUID_DEFINED
 #else
 typedef struct _LIST_ENTRY LIST_ENTRY;
 
@@ -9,4 +11,13 @@ struct _LIST_ENTRY {
     LIST_ENTRY* ForwardLink;
     LIST_ENTRY* BackLink;
 };
+
+typedef void VOID;
+
+typedef struct _GUID {
+    unsigned long  Data1;
+    unsigned short Data2;
+    unsigned short Data3;
+    unsigned char  Data4[8];
+} GUID;
 #endif // _WIN32
